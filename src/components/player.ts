22fa24f3beeca7713b607128320602b759c75739
Zhake <ogremagizk4@gmail.com>
2025-03-01 23:45:44 +0500

    added player & gravity

---
+++ src/components/player.ts
@@ -0,0 +1,43 @@
+import { Application, Graphics } from "pixi.js";
+
+interface PlayerI {
+  app: Application;
+  velocityX: number;
+  velocityY: number;
+}
+
+class Player extends Graphics {
+  app: Application;
+  velocityX;
+  velocityY;
+  gravity: number = 0.5;
+
+  constructor(options: PlayerI) {
+    super();
+    this.app = options.app;
+    this.velocityX = options.velocityX;
+    this.velocityY = options.velocityY;
+  }
+
+  draw() {
+    this.rect(0, 0, 100, 100).fill({
+      color: "red",
+      alpha: 1,
+    });
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocityY;
+
+    // остановка плейера, если достигает низа
+    if (
+      this.position.y + this.height + this.velocityY <=
+      this.app.canvas.height
+    ) {
+      this.velocityY += this.gravity;
+    } else this.velocityY = 0;
+  }
+}
+
+export { Player };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { Application } from "pixi.js";
+import { Player } from "./components/player";
 
 (async () => {
   const app = new Application();
   await app.init({ background: "#1099bb", resizeTo: window });
   document.body.appendChild(app.canvas);
+
+  const player = new Player({ app, velocityX: 0, velocityY: 0 });
+  player.position.set(100, 100);
+  app.stage.addChild(player);
+
+  app.ticker.add(() => {
+    player.update();
+  });
 })();
